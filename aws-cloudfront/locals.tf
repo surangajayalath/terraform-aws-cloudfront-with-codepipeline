b05d281ff4eb9122b4873f9c8cf320370ebf0b8b
suranga-jayalath <suranga@shoutoutlabs.io>
2025-03-11 18:20:00 +0530

    tf files

--- aws-cloudfront/locals.tf
+++ aws-cloudfront/locals.tf
@@ -2,5 +2,5 @@
 
 locals {
   s3_origin_id    = "s3OriginCloudFront"
-  domain_aliases  = ["${var.domain_name}.${var.domain_name}", "www.${var.domain_name}.${var.domain_name}"]
+  domain_aliases  = ["${var.domain_name}", "www.${var.domain_name}"]
 }
