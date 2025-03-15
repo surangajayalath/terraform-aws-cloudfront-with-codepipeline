dbe4995d2db9bb7d439480213d961901d5e5c9c8
suranga-jayalath <suranga@shoutoutlabs.io>
2025-03-15 12:09:37 +0530

    cdn module

--- aws-cloudfront/variables.tf
+++ aws-cloudfront/variables.tf
@@ -113,6 +113,7 @@ variable "compute_type" {
 variable "image" {
   description = "Docker image for the CodeBuild environment."
   type        = string
+  default = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
 }
 
 variable "image_type" {
